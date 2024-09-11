8083f6db1eee8533bab2f5c0984cda5b8ef1c7a0
IdkTheSecond <bjarnetitan3@gmail.com>
2024-09-11 16:19:32 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -257,6 +257,7 @@ Tabs.Sniper:AddButton({
 		if sniping == true then
 			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = snipeAreas[snipeArea][1]
 			repeat
+				wait(1)
 				repeat
 					game:GetService("ReplicatedStorage").FindPokemon:InvokeServer("WildGrass")
 				until #game:GetService("Players").LocalPlayer.OppPokemon:GetChildren() > 0
